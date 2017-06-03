7735d9a9ca44281926ba7b0e204223ec62970658
Michael Dove <michael@michaeldove.com.au>
2017-06-03 04:33:38 -0700

    Handle ignition, rpm, load and maf measuring blocks.

--- vagvehicle.py
+++ vagvehicle.py
@@ -17,10 +17,29 @@ MY_ADDRESS = 0x32eL
 MY_CHANNEL_ADDRESS = 0x07
 MY_CHANNEL_ADDRESS_DOUBLE = 0x740L
 
+UNKNOWN_ADDRESS_DOUBLE = 0x710L # Broadcast?
+
 KWP_READ_ECU_ID = 0x1a
 KWP_ECU_ID_VAG_NUMBER_3 = 0x9a
 COMPONENT_ID= 0x9b
 ECU_ID = 0x91
+
+KWP_READ_MEASURING_BLOCK = 0x21
+KWP_MEASURING_BLOCK_1 = 0x02
+KWP_MEASURING_BLOCK_20 = 0x14
+KWP_MEASURING_BLOCK_115 = 0x73
+
+RPM_SCALE_FACTOR = 40
+INJECTION_TIMING_SCALE_FACTOR = 255 # usecs / 255
+UNIT_PERCENT = 0x21
+UNIT_PERCENT_2 = 0x17
+UNIT_DEGREES = 0x22
+UNIT_MS = 0x16
+UNIT_GS = 0x19
+LOAD_PRESCALER = 0x85
+RPM_PRESCALER = 0xc8
+INJECTION_TIMING_PRESCALER = 0xff
+MAF_PRESCALER = 0x02
 
 remote_arbitration_id = None
 controller = None
@@ -28,8 +47,29 @@ kwp_resp_seq = 0
 
 bus = can.interface.Bus(channel='slcan0', bustype='socketcan')
 
+# See VAG-Blocks Github project for scaling
+def scale_rpm(rpm):
+    return min(rpm / RPM_SCALE_FACTOR, 0xff)
+
+def scale_injection_timing(timing):
+    return [INJECTION_TIMING_SCALE_FACTOR, min(
+        int(round(timing / float(INJECTION_TIMING_SCALE_FACTOR))),
+        0xff)]
+
+def scale_load(load):
+    return [LOAD_PRESCALER, min(
+        int(round(load / (100.0/LOAD_PRESCALER))),
+        0xff)]
+
+def scale_maf(maf):
+    return [MAF_PRESCALER, min(
+        int(round(maf / (100.0/MAF_PRESCALER))),
+        0xff)]
+
+
+
 def send(message):
-	time.sleep(0.0005)
+	time.sleep(0.0055)
 	try:
 		bus.send(message)
 		print("Message sent {}".format(message))
@@ -40,18 +80,54 @@ def str_to_bytes(text):
     import array
     return array.array('B', text)
 
-def chunk(arr, n):
+def chunk(arr, n=7):
     for i in xrange(0, len(arr), n):
         yield arr[i:i + n]
-    
 
-def channel_handler(message):
+def message(arbitration_id, data):
+    return can.Message(arbitration_id=arbitration_id,
+                      data=data,
+                      extended_id=False)
+    
+def data_send(payload):
+    """Send payload within a VWT Data Message."""
+    global kwp_resp_seq
+    data = [0x00, len(payload)] + payload
+    segmented_data = list(chunk(data))
+    for seq, msg_data in enumerate(segmented_data):
+        sequence_code = 0x20
+        is_last_seq = seq == len(segmented_data) - 1
+        if is_last_seq:
+            sequence_code = 0x10
+        resp_opcode = sequence_code | kwp_resp_seq
+        msg = message(remote_arbitration_id, [resp_opcode] + msg_data)
+        kwp_resp_seq = (kwp_resp_seq + 1) & 0xf
+        send(msg)
+    ack = bus.recv()
+    expected_ack = 0xb0 | kwp_resp_seq
+    if ack.data[0] == expected_ack:
+        print("Message acknowledged by tester.")
+    else:
+        print("Expecting ack {} but got {}".format(hex(expected_ack), ack))
+
+def send_ack(last_seq):
+    """Send VWTP acknowledgement packet."""
+    seq = (last_seq + 1) & 0x0f
+    resp_opcode = 0xb0 | seq
+    msg = message(remote_arbitration_id, [resp_opcode])
+    send(msg)
+
+def null_handler(in_message):
+    global remote_arbitration_id
+    remote_arbitration_id = None
+
+def channel_handler(in_message):
     global kwp_resp_seq, remote_arbitration_id
-    opcode = message.data[0]
+    opcode = in_message.data[0]
     if opcode == CHANNEL_PARAMS_REQ:
-        msg = can.Message(arbitration_id=remote_arbitration_id,
-						  data=[CHANNEL_PARAMS_RESP, 0x0f, 0x8a, 0xff, 0x4a, 0xff],
-						  extended_id=False)
+        msg = message(
+            remote_arbitration_id,
+            [CHANNEL_PARAMS_RESP, 0x0f, 0x8a, 0xff, 0x4a, 0xff])
         send(msg)
         print("Send channel params response")
     elif opcode == CHANNEL_CLOSE_REQ:
@@ -59,184 +135,145 @@ def channel_handler(message):
         kwp_resp_seq = 0
         remote_arbitration_id = None
     elif opcode & 0xf0 == 0x10: # Data packet
-        kwp_opcode = message.data[3]
-        ecu_id_type = message.data[4] 
+        kwp_opcode = in_message.data[3]
+        ecu_id_type = in_message.data[4] 
         req_seq = opcode & 0x0f
         if kwp_opcode == KWP_READ_ECU_ID:
             data = None
             if ecu_id_type == COMPONENT_ID:
-                data = [[0x5a, COMPONENT_ID, 0x38, 0x50, 0x30],
-                        [0x39, 0x30, 0x37, 0x31, 0x31, 0x35, 0x41],
-                        [0x51, 0x20, 0x30, 0x30, 0x31, 0x30, 0x10],
-                        [0x00, 0x00, 0x00, 0x01, 0xdc, 0x5a, 0x10],
-                        [0x00, 0xbf, 0x32, 0x2e, 0x30, 0x6c, 0x20],
-                        [0x52, 0x34, 0x2f, 0x34, 0x56, 0x20, 0x54],
-                        [0x46, 0x53, 0x49, 0x20, 0x20, 0x20, 0x20],
-                        [0x20]]
+                payload = [0x5a, COMPONENT_ID, 0x38, 0x50, 0x30,
+                        0x39, 0x30, 0x37, 0x31, 0x31, 0x35, 0x41,
+                        0x51, 0x20, 0x30, 0x30, 0x31, 0x30, 0x10,
+                        0x00, 0x00, 0x00, 0x01, 0xdc, 0x5a, 0x10,
+                        0x00, 0xbf, 0x32, 0x2e, 0x30, 0x6c, 0x20,
+                        0x52, 0x34, 0x2f, 0x34, 0x56, 0x20, 0x54,
+                        0x46, 0x53, 0x49, 0x20, 0x20, 0x20, 0x20,
+                        0x20]
             elif ecu_id_type == ECU_ID:
-                data = str_to_bytes("Underground")
-                data = [[0x5a, ECU_ID, 0x0e, 0x38, 0x50],
-                        [0x30, 0x39, 0x30, 0x37, 0x31, 0x31, 0x35],
-                        [0x42, 0x20, 0x20, 0x20, 0xff]]
+                #data = str_to_bytes("Underground")
+                payload = [0x5a, ECU_ID, 0x0e, 0x38, 0x50,
+                        0x30, 0x39, 0x30, 0x37, 0x31, 0x31, 0x35,
+                        0x42, 0x20, 0x20, 0x20, 0xff]
             elif ecu_id_type == KWP_ECU_ID_VAG_NUMBER_3:
-                data = [[0x5a, 0x9a, 0x01, 0xdc, 0x5a],
-                        [0x10, 0x00, 0xbf, 0x30, 0x30, 0x31, 0x30],
-                        [0x10, 0x09, 0x01, 0x03, 0x00, 0x0c, 0x18],
-                        [0x0f, 0x01, 0x60, 0xff]]
+                payload = [0x5a, 0x9a, 0x01, 0xdc, 0x5a,
+                        0x10, 0x00, 0xbf, 0x30, 0x30, 0x31, 0x30,
+                        0x10, 0x09, 0x01, 0x03, 0x00, 0x0c, 0x18,
+                        0x0f, 0x01, 0x60, 0xff]
             else:
                 import pdb;pdb.set_trace()
 
             if req_seq > 0:
-                seq = (req_seq + 1) & 0x0f
-                resp_opcode = 0xb0 | seq
-                msg = can.Message(arbitration_id=remote_arbitration_id,
-                                  data=[resp_opcode],
-                                  extended_id=False)
-                send(msg)
+                send_ack(req_seq)
+            data_send(payload)
 
-
-            for i in range(len(data)):
-                sequence_code = 0x20
-                if i == len(data) -1:
-                    sequence_code = 0x10
-                data_row = data[i]
-                if i == 0:
-                    flatten = lambda l: [item for sublist in l for item in sublist] 
-                    payload_length = len(flatten(data))
-                    data_row = [0x00, payload_length] + data_row
-                resp_opcode = sequence_code | kwp_resp_seq
-                msg = can.Message(arbitration_id=remote_arbitration_id,
-                                  data=[resp_opcode] + data_row,
-                                  extended_id=False)
-                kwp_resp_seq = (kwp_resp_seq + 1) & 0xf
-                send(msg)
-            ack = bus.recv()
-            if ack.data[0] == (0xb0 & kwp_resp_seq):
-                print("Message acknowledged by tester.")
-            else:
-                print("Expecting ack but got {}".format(message))
         elif kwp_opcode == 0x10 and ecu_id_type == 0x89:
-            seq = (req_seq + 1) & 0x0f
-            resp_opcode = 0xb0 | seq
-            msg = can.Message(arbitration_id=remote_arbitration_id,
-                              data=[resp_opcode],
-                              extended_id=False)
-            send(msg)
-
-            data = [[0x00, 0x02, 0x50, 0x89]]
-
-            for i in range(len(data)):
-                sequence_code = 0x20
-                if i == len(data) -1:
-                    sequence_code = 0x10
-                resp_opcode = sequence_code | kwp_resp_seq
-                msg = can.Message(arbitration_id=remote_arbitration_id,
-                                  data=[resp_opcode] + data[i],
-                                  extended_id=False)
-                kwp_resp_seq = (kwp_resp_seq + 1) & 0xf
-                send(msg)
-            ack = bus.recv()
-            if ack.data[0] == (0xb0 & kwp_resp_seq):
-                print("Message acknowledged by tester.")
-            else:
-                print("Expecting ack but got {}".format(message))
-
+            send_ack(req_seq)
+            payload = [0x50, 0x89]
+            data_send(payload)
+            
         elif kwp_opcode == 0x31 and ecu_id_type == 0xb8: # basic measuring blocks + more
-            seq = (req_seq + 1) & 0x0f
-            resp_opcode = 0xb0 | seq
-            msg = can.Message(arbitration_id=remote_arbitration_id,
-                              data=[resp_opcode],
-                              extended_id=False)
-            send(msg)
+            send_ack(req_seq)
+            payload = [0x71, 0xb8, 0x01, 0x01, 0x01,
+                       0x03, 0x01, 0x02, 0x01, 0x06, 0x01, 0x07,
+                       0x01, 0x08, 0x01, 0x0d, 0x01, 0x18]
+            data_send(payload)
+        elif kwp_opcode == KWP_READ_MEASURING_BLOCK: # advanced measuring block
+            send_ack(req_seq)
+            if ecu_id_type == KWP_MEASURING_BLOCK_1: # RPM, load, injection timing, MAF
+                rpm = 3000
+                injection_timing = 2000 #usecs
+                load = 180
+                maf = 2
 
-            data = [[0x00, 0x12, 0x71, 0xb8, 0x01, 0x01, 0x01],
-                    [0x03, 0x01, 0x02, 0x01, 0x06, 0x01, 0x07],
-                    [0x01, 0x08, 0x01, 0x0d, 0x01, 0x18]]
-
-            for i in range(len(data)):
-                sequence_code = 0x20
-                if i == len(data) -1:
-                    sequence_code = 0x10
-                resp_opcode = sequence_code | kwp_resp_seq
-                msg = can.Message(arbitration_id=remote_arbitration_id,
-                                  data=[resp_opcode] + data[i],
-                                  extended_id=False)
-                kwp_resp_seq = (kwp_resp_seq + 1) & 0xf
-                send(msg)
-            ack = bus.recv()
-            if ack.data[0] == (0xb0 & kwp_resp_seq):
-                print("Message acknowledged by tester.")
-            else:
-                print("Expecting ack but got {}".format(message))
-
-
+                scaled_rpm = scale_rpm(rpm)
+                scaled_injection_timing = scale_injection_timing(injection_timing)
+                scaled_load = scale_load(load)
+                scaled_maf = scale_maf(maf)
+                payload = [0x61, 0x02, 0x01, RPM_PRESCALER, scaled_rpm,
+                           UNIT_PERCENT, scaled_load[0], scaled_load[1],
+                           UNIT_MS, scaled_injection_timing[0], scaled_injection_timing[1],
+                           UNIT_GS, scaled_maf[0], scaled_maf[1],
+                           0x25, 0x00, 0x00,
+                           0x25, 0x04, 0x1d,
+                           0x25, 0x02, 0x7a,
+                           0x25, 0x00, 0x00]
+                data_send(payload)
+            elif ecu_id_type == KWP_MEASURING_BLOCK_20:
+                payload = [0x61, KWP_MEASURING_BLOCK_20,
+                           UNIT_DEGREES, 0x4b, 0xff,
+                           UNIT_DEGREES, 0x4b, 0x80,
+                           UNIT_DEGREES, 0x4b, 0x80,
+                           UNIT_DEGREES, 0x4b, 0x80,
+                           0x25, 0x00, 0x00,
+                           0x25, 0x00, 0x00,
+                           0x25, 0x00, 0x00,
+                           0x25, 0x00, 0x00]
+                data_send(payload)
+            elif ecu_id_type == KWP_MEASURING_BLOCK_115:
+                payload = [0x61, KWP_MEASURING_BLOCK_115,
+                           0x01, 0xc8, 0x12,
+                           0x21, 0x85, 0x19,
+                           0x12, 0xfa, 0x22,
+                           0x60, 0x64, 0xff, #65
+                           0x36, 0x05, 0x02,
+                           0x36, 0x03, 0x1e, 
+                           0x36, 0x04, 0x22,
+                           0x36, 0x04, 0xc7]
+                data_send(payload)
+                
 
     elif opcode == 0xa3: #keep alive packet
-        msg = can.Message(arbitration_id=remote_arbitration_id,
-                          data=[0xa1, 0x0f, 0x8a, 0xff, 0x4a, 0xff],
-                          extended_id=False)
+        msg = message(remote_arbitration_id, [0xa1, 0x0f, 0x8a, 0xff, 0x4a, 0xff])
         send(msg)
     else:
         print("Unknown opcode {}".format(hex(opcode)))
 
-def set_remote_arbitration_id(message):
+def set_remote_arbitration_id(in_message):
 	global remote_arbitration_id
-	remote_logical_id = message.data[5]
+	remote_logical_id = in_message.data[5]
 	remote_arbitration_id = remote_logical_id << 8
 
-def setup_channel(message):
+def setup_channel(in_message):
     print("Setting up channel")
     if remote_arbitration_id is None:
-        set_remote_arbitration_id(message)
-        remote_logical_id = message.data[5]
-        msg = can.Message(arbitration_id=OPEN_CONTROLLER_ADDRESS_RESP,
-						  data=[0, 0xd0,
-								0x00, remote_logical_id,
-								0x40, MY_CHANNEL_ADDRESS, 0x01],
-						  extended_id=False)
-        print(msg)
+        set_remote_arbitration_id(in_message)
+        remote_logical_id = in_message.data[5]
+        data = [0x00, 0xd0, 0x00, remote_logical_id, 0x40, MY_CHANNEL_ADDRESS, 0x01]
+        msg = message(OPEN_CONTROLLER_ADDRESS_RESP, data)
         send(msg)
     else:
         print("I already have an arbitration ID, maybe channel is already setup.")
 
-def controller_ident_handler(message):
-    set_remote_arbitration_id(message)
-    msg = can.Message(arbitration_id=0x021f,
-						  data=[0x00, 0xd0, 0x00, 0x03, 0x2e, 0x03, 0x01],
-						  #data=[0xa1, 0x0f, 0x8a, 0xff, 0x4a, 0xff],
-						  extended_id=False)
+def controller_ident_handler(in_message):
+    set_remote_arbitration_id(in_message)
+    msg = message(0x021f, [0x00, 0xd0, 0x00, 0x03, 0x2e, 0x03, 0x01])
     send(msg)
 
-def engine_controller_handler(message):
-	opcode = message.data[1]
+def engine_controller_handler(in_message):
+	opcode = in_message.data[1]
 	if opcode == CHANNEL_SETUP_OPCODE:
-		setup_channel(message)
+		setup_channel(in_message)
 		controller = ENGINE_CONTROLLER
 
-def open_controller_handler(message):
-	controller_id = message.data[0]
-	controller_handlers[controller_id](message)
+def open_controller_handler(in_message):
+	controller_id = in_message.data[0]
+	controller_handlers[controller_id](in_message)
 
 	
 
-def handle_message(message):
-	print("Received message {}".format(message))
-	print ("Arbitration ID: {}".format(hex(message.arbitration_id)))
-	arbitration_handlers[message.arbitration_id](message)
+def handle_message(in_message):
+	#print("Received message {}".format(message))
+	#print ("Arbitration ID: {}".format(hex(message.arbitration_id)))
+	arbitration_handlers[in_message.arbitration_id](in_message)
 
 def receive_message():
-	message = bus.recv()
-	handle_message(message)
+	in_message = bus.recv()
+	handle_message(in_message)
 	
-def send_one():
-	msg = can.Message(arbitration_id=0xc0ffee,
-					  data=[0, 25, 0, 1, 3, 1, 4, 1],
-					  extended_id=True)
-	send(msg)
-
 arbitration_handlers = {OPEN_CONTROLLER_ADDRESS : open_controller_handler,
 						MY_ADDRESS : channel_handler,
-                        MY_CHANNEL_ADDRESS_DOUBLE: channel_handler }
+                        MY_CHANNEL_ADDRESS_DOUBLE: channel_handler,
+                        UNKNOWN_ADDRESS_DOUBLE: null_handler }
 controller_handlers = {ENGINE_CONTROLLER : engine_controller_handler,
 					   CONTROLLER_IDENT_REQ: controller_ident_handler}
 
